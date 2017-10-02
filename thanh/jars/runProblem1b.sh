d387e318384499581414f12f3c7aa8f6003420dc
hadoop <hadoop@hadoop-VirtualBox.(none)>
2017-10-02 19:27:39 -0400

    push files

---
+++ thanh/jars/runProblem1b.sh
@@ -0,0 +1,11 @@
+INPUT_PATH="/user/hadoop/input/project2/test/"
+OUTPUT_PATH="/user/hadoop/output/project2/test"
+#if [ $# -eq 0 ]
+#then
+#    echo "Usage: input Jar file path"
+#    exit 1
+#fi
+hadoop fs -rmr -skipTrash $OUTPUT_PATH
+hadoop jar MapReduceRunner.jar 1b $INPUT_PATH $OUTPUT_PATH
+echo "Result: \n"
+hadoop fs -cat $OUTPUT_PATH/part*
